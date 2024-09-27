b86269d2ddb724ed22851b121a967feb3985df69
imc <i_cenov@botevgrad.com>
2024-09-27 20:19:07 +0300

    Files removed from git control

--- test4.py
+++
@@ -1,9 +0,0 @@
-
-import mqttms
-from mqttms import run_module
-import mqttms.logger_module
-from mqttms.logger_module import logger
-
-logger.info(f"test4.py begin")
-run_module()
-logger.info(f"test4.py end")
